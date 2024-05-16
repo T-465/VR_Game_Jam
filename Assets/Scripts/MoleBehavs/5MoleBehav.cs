9e9864005b7e26fd63fa049350115af9f0967d7e
Naoishe <McAleese-N6@ulster.ac.uk>
2024-05-16 15:42:02 +0100

    My Scene and prefabs with wackamole

---
+++ Assets/Scripts/MoleBehavs/5MoleBehav.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VMoleBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject TriggerScrpt;
+    private bool readOn;
+    private bool repeatPattern;
+    private bool allowBegin;
+    private Vector3 startingPosition;
+
+    private void Awake()
+    {
+        repeatPattern = false;
+        startingPosition = this.transform.position;
+        StartCoroutine(InitialDelay());
+    }
+    private void Update()
+    {
+        if (allowBegin)
+        {
+            readOn = TriggerScrpt.GetComponent<MoleTrigger>().ButtonPressedOn;
+            if (readOn == true)
+            {
+                if (repeatPattern == false)
+                {
+
+                    repeatPattern = true;
+                    StartCoroutine(Pattern());
+
+                }
+            }
+            else
+            {
+                repeatPattern = false;
+            }
+        }
+
+    }
+
+    private IEnumerator Pattern()
+    {
+        yield return new WaitForSeconds(0.1f);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0f, 3f, 0f);
+        yield return new WaitForSeconds(0.3f);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        yield return new WaitForSeconds(0.5f);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0f, -3f, 0f);
+        yield return new WaitForSeconds(0.3f);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        //Debug.Log("Pattern Finished");
+        if (repeatPattern)
+        {
+            StartCoroutine(Pattern());
+        }
+
+    }
+
+    private IEnumerator InitialDelay()
+    {
+        yield return new WaitForSeconds(2);
+        allowBegin = true;
+
+    }
+
+
+
+}
+
